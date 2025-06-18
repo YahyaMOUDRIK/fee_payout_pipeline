4c997d2d48dd374822d3d5defba99cf7cb0c430b
Yahya Moudrik <yahya.moudrik.00@gmail.com>
2025-06-18 12:08:28 +0100

    updated connection to db using env variables

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,6 +1,10 @@
 ''' File for database utilities : Connections, queries, etc. '''
 import yaml
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def connect_to_database(db_config_yaml):
 
@@ -9,9 +13,13 @@ def connect_to_database(db_config_yaml):
         server = config["server"]
         database = config["database"]
         driver = config["driver"]
+        username = os.getenv('username')
+        password = os.getenv('password')
         trusted_connection = config["trusted_connection"]
     try:
         connection = pyodbc.connect(f'Driver={driver};'
+                        f'UID={username};'
+                        f'PWD={password};'
                         f'SERVER={server};'
                         f'Database={database};'
                         f'Trusted_Connection={trusted_connection};')
@@ -20,3 +28,19 @@ def connect_to_database(db_config_yaml):
     except pyodbc.Error as e:
         print(f"Error connecting to database: {e}")
         return None
+    
+def test_connection(db_config_yaml):
+    connection = connect_to_database(db_config_yaml)
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT 1")
+            print("Connection test successful.")
+        except pyodbc.Error as e:
+            print(f"Error during connection test: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        print("Failed to connect to the database.")
+
